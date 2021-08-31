38f59104bee6701be5ef194b455fe72f6c1bc1a3
王征 <zheng.wang@k7.cn>
2021-08-31 21:08:10 +0800

    完善peg计算方式

--- investment/stock/Zulu.py
+++ investment/stock/Zulu.py
@@ -1,3 +1,8 @@
+import json
+import os
+import pickle
+import time
+import requests
 import tushare
 pro = tushare.pro_api("b625f0b90069039346d199aa3c0d5bc53fd47212437337b45ba87487")
 
@@ -43,7 +48,7 @@ class Stock_BaseInfo:
 
     @staticmethod
     def growth_run():
-        data = pro.bak_basic(trade_date='20191231')
+        data = pro.bak_basic(trade_date='20210331')
         for i in data.values:
             if 0 < int(i[16]) < 20200101:
                 s = Stock_BaseInfo()
@@ -65,9 +70,35 @@ class Stock_BaseInfo:
                 s.peg = round(s.pe / s.profit_yoy, 2) if s.profit_yoy != 0 else 0
                 # s.one_year_yield = s.get_yield()
                 # s.one_month_yield = s.get_yield(30)
-                if 0 < s.pe and 0 < s.peg:
-                    print(f"代码:{s.code}\t名称:{s.name}\tPE:{s.pe}\t每股收益:{s.eps}\t利润同比:{s.profit_yoy}\t收入同比:{s.rev_yoy}\tPEG:{s.peg}\t")  # 一年涨幅:{s.one_year_yield}\t一月涨幅:{s.one_month_yield}
+                print(f"代码:{s.code}\t名称:{s.name}\tPE:{s.pe}\t每股收益:{s.eps}\t利润同比:{s.profit_yoy}\t收入同比:{s.rev_yoy}\tPEG:{s.peg}\t")  # 一年涨幅:{s.one_year_yield}\t一月涨幅:{s.one_month_yield}
 
 
-Stock_BaseInfo().growth_run()
+def get_annual_report_to_file():
+    # 获取所有上市公司年报数据并写入文件
+    year = 2019
+    pageSize = 50
+    annual_report_list = []
+    for pageNumber in range(1, 234):
+        time.sleep(0.5)
+        timestamp = int(time.time()*1000)
+        callback = f"jQuery112305629597608570047_{timestamp}"
+        url = f"http://datacenter-web.eastmoney.com/api/data/get?callback={callback}&st=UPDATE_DATE,SECURITY_CODE&sr=-1,-1&ps={pageSize}&p={pageNumber}&type=RPT_LICO_FN_CPD&sty=ALL&token=894050c76af8597a853f5b408b759f5d&filter=(REPORTDATE='{year}-12-31')"
+        response = requests.get(url)
+        response = response.text.replace(f"{callback}(", '')[:-2]
+        response = json.loads(response)
+        if 'result' in response.keys():
+            response = response.get('result')
+            if 'data' in response.keys():
+                response = response.get('data')
+                if response:
+                    for i in response:
+                        if i['SECURITY_CODE'][0] in ('0', '3', '6'):
+                            annual_report_list.append(i)
+    target_file = f"annual_report_{year}.bin"
+    if target_file in os.listdir(os.curdir):
+        os.remove(target_file)
+    with open(target_file, 'wb') as f:
+        f.write(pickle.dumps(annual_report_list))
 
+
+get_annual_report_to_file()
--- investment/stock/research_report.py
+++ investment/stock/research_report.py
@@ -1,10 +1,13 @@
 import time
+import pickle
 import requests
 import json
 from datetime import date, timedelta
+from security import get_stock_kline_with_volume
 
 
 def get_research_report(code):
+    # 获取个股研报数据
     beginTime = date.today() - timedelta(days=60)
     endTime = date.today()
     timestamp = int(time.time()*1000)
@@ -31,20 +34,42 @@ def get_predict_eps(code):
     return avg_predictThisYearEps, avg_predictNextYearEps
 
 
-def calculate_peg(code, close_price, this_year_eps, last_year_eps):
-    thisYearWeight = 6  # 今年剩下的月数
-    nextYearWeight = 6  # 未来12个月中明年所占的月数
+def select_stock_last_year_eps(code):
+    # 查询个股上一年度的每股收益
+    years = ['2019', '2020']
+    eps = {'2019': None, '2020': None}
+    for year in years:
+        target_file = f"annual_report_{year}.bin"
+        with open(target_file, 'rb') as f:
+            file = f.read()
+            content = pickle.loads(file)
+        for i in content:
+            if i.get('SECURITY_CODE') == str(code):
+                eps[year] = i.get('BASIC_EPS')
+                # eps[f"{year}_deduct"] = i.get('DEDUCT_BASIC_EPS')
+    return eps
+
+
+def calculate_peg(code):
+    year = str(date.today().year - 1)
+    history_eps = select_stock_last_year_eps(code)
+    last_year_eps = history_eps.get(year)
+    close_price = get_stock_kline_with_volume(code, limit=5)[-1]['close']
+    thisYearWeight = 4  # 今年剩下的月数
+    nextYearWeight = 12 - thisYearWeight  # 未来12个月中明年所占的月数
     avg_predictThisYearEps, avg_predictNextYearEps = get_predict_eps(code)
     predict_the_coming_year_eps = round(avg_predictThisYearEps*nextYearWeight/12 + avg_predictNextYearEps*thisYearWeight/12, 2)
     print(f"预测未来一年的收益: {predict_the_coming_year_eps}")
     predict_pe = round(close_price/predict_the_coming_year_eps, 2)
     print(f"预测未来一年的市盈率: {predict_pe}")
-    past_year = round(this_year_eps*nextYearWeight/12 + last_year_eps*thisYearWeight/12, 2)
+    past_year = round(avg_predictThisYearEps*nextYearWeight/12 + last_year_eps*thisYearWeight/12, 2)
     print(f"过去一年的收益: {past_year}")
     growth_rate_earnings_per_share = round((predict_the_coming_year_eps - past_year)/past_year * 100, 2)
-    print(f"未来每股收益增长率: {growth_rate_earnings_per_share}")
+    print(f"未来每股收益增长率: {growth_rate_earnings_per_share}%")
     peg = round(predict_pe/growth_rate_earnings_per_share, 2)
     print(f"peg: {peg}")
 
 
-calculate_peg(601636, 28.17, 0.82, 0.46)
+calculate_peg(601636)
+
+
