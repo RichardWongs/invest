61614547c0ee9492ed2edf45d1790544b0277c16
RichardWongs <mailofwangzheng@163.com>
2021-09-13 00:03:36 +0800

    run code

--- investment/monitor/abnormal_volume_monitoring.py
+++ investment/monitor/abnormal_volume_monitoring.py
@@ -153,7 +153,7 @@ def peg_stock_monitor():
 
 
 if __name__ == '__main__':
-    # sending_today_stock_pool()
+    sending_today_stock_pool()
     last_week_foreign_capital_add()
     # peg_stock_monitor()
     # from RPS.bak_stock_pool import finally_pool
---
+++ investment/stock/strong.py
@@ -0,0 +1,23 @@
+from security import get_stock_kline_with_volume
+# 笑傲牛熊, 曼斯菲尔德相对强度
+
+
+def index_applies():
+    indexes = ['000300', '000905', '399006', '000688']
+    applies_120 = 0
+    result = []
+    for index in indexes:
+        data120 = get_stock_kline_with_volume(index, is_index=True, limit=120)
+        pre, current = data120[0]['close'], data120[-1]['close']
+        if applies_120 < current/pre:
+            applies_120 = current/pre
+            result = data120
+    return result if result else None
+
+
+def GET_RSMANSIFIELD(code):
+    index = index_applies()
+    index_applies_list = [i['applies'] for i in index]
+    data120 = get_stock_kline_with_volume(code, limit=120)
+    stock_applies_list = [i['applies'] for i in data120]
+
