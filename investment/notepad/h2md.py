0108d30fce0342831dda0a7fb87825db063d140e
王征 <zheng.wang@k7.cn>
2021-11-11 18:27:31 +0800

    run code

---
+++ investment/notepad/h2md.py
@@ -0,0 +1,56 @@
+import logging
+import random
+import string
+import html2text
+import os
+import requests
+
+path = r"C:\Users\Administrator\Desktop\ETF\陶博士"
+if not os.path.exists(path):
+    os.makedirs(path)
+os.chdir(path)
+
+
+def internet_request(url, filename=None):
+    if not filename:
+        filename = "".join(random.sample(string.ascii_uppercase, 6)) + ".html"
+    else:
+        filename = filename + ".html"
+    try:
+        response = requests.get(url)
+        with open(filename, 'w', encoding="utf-8") as f:
+            f.write(response.text)
+    except Exception() as e:
+        logging.error(e)
+    return filename
+
+
+def html2md(filename):
+    with open(filename, 'r', encoding="utf-8") as f:
+        content = f.read()
+        md_content = html2text.html2text(content)
+    filename_md = md_content.split('\n')[0].replace('##  ', '') + ".md"
+    with open(filename_md, 'w', encoding="utf-8") as f:
+        f.write(md_content)
+    os.remove(filename)
+
+
+def run(url, filename=None):
+    filename = internet_request(url, filename=filename)
+    html2md(filename)
+
+
+if __name__ == "__main__":
+    urls =[
+        "https://mp.weixin.qq.com/s/WoVl_byT47opLAcqxnheDw",
+        "https://mp.weixin.qq.com/s/kBFxwkl0-rKxYi8RNZroTw",
+        "https://mp.weixin.qq.com/s/cGciypZ4r06Ft0vKxyfFuQ",
+        "https://mp.weixin.qq.com/s/r7QfPQaeOCwbOoZWLykwJw",
+        "https://mp.weixin.qq.com/s/8cJiHst58ux5Ox2AUZVHYg",
+        "https://mp.weixin.qq.com/s/wvOLfwSGrVTg6CI5GzjDaQ"
+    ]
+    for url in urls:
+        run(url)
+
+
+
