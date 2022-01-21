9f5ffa9827c704105a16834ebca4725b8b3efa04
王征 <zheng.wang@k7.cn>
2022-01-21 10:22:22 +0800

    run update market

--- investment/RPS/动量模型.py
+++ investment/RPS/动量模型.py
@@ -10,7 +10,7 @@ from RPS.stock_pool import NEW_STOCK_LIST
 from monitor import get_market_data, get_stock_kline_with_indicators, MA_V2, get_industry_by_code
 from monitor.whole_market import RedisConn
 from momentum.concept import get_concept_list, get_industry_list, get_concept_kline, select_composition_stock
-start_date = int(str(date.today()-timedelta(days=400)).replace('-', ''))
+start_date = int(str(date.today() - timedelta(days=400)).replace('-', ''))
 host = "172.16.1.162"
 # host = "192.168.124.20"
 
@@ -31,9 +31,11 @@ def saveMarketData2Redis():
             if kline:
                 if len(kline) > 0:
                     if len(kline) > 20:
-                        v['applies_20'] = round((kline[-day]['close']-kline[-(N+day)]['close'])/kline[-(N+day)]['close'], 2)
+                        v['applies_20'] = round(
+                            (kline[-day]['close'] - kline[-(N + day)]['close']) / kline[-(N + day)]['close'], 2)
                     else:
-                        v['applies_20'] = round((kline[-day]['close']-kline[0]['close'])/kline[0]['close'], 2)
+                        v['applies_20'] = round(
+                            (kline[-day]['close'] - kline[0]['close']) / kline[0]['close'], 2)
                     v['kline'] = kline
                     client.set(f"stock:momentum:{k}", json.dumps(v))
                 print(counter, v)
@@ -84,7 +86,7 @@ def find_institutions_holding():
 def find_high_rps_stock():
     # 剔除20日RPS强度小于85的个股
     content = readMarketDataFromLocal()
-    stockCount = int(len(content)*0.15)
+    stockCount = int(len(content) * 0.15)
     result = []
     for i in content:
         del i['kline']
@@ -97,7 +99,8 @@ def find_new_stock(pool):
     # 剔除上市不足一个月的新股
     target = []
     for i in pool:
-        if int(i['list_date']) < int(str(date.today()-timedelta(30)).replace('-', '')):
+        if int(i['list_date']) < int(
+                str(date.today() - timedelta(30)).replace('-', '')):
             target.append(i)
     return target
 
@@ -135,7 +138,8 @@ def find_trend_pool():
     from stock.StockFinancial import read_quarter_report
     quarter = read_quarter_report()  # 季报正增长
     for i in quarter:
-        i['code'] = f"{i['code']}.SH" if i['code'].startswith('6') else f"{i['code']}.SZ"
+        i['code'] = f"{i['code']}.SH" if i['code'].startswith(
+            '6') else f"{i['code']}.SZ"
     tmp = find_institutions_holding()
     ins = []    # 机构&外资持股
     for _, v in tmp.items():
@@ -166,9 +170,11 @@ def run():
     for i in rps_pool:
         if i['code'] in institution_pool.keys():
             result.append(i)
+    logging.warning(f"机构动量榜详情: {len(result)}\t{result}")
     all_new_high = week52_new_high()
     institution_new_high = [i for i in result if i in all_new_high]
-    logging.warning(f"全市场创新高个股数量:{len(all_new_high)}\t机构动量榜个股数量:{len(result)}\t创新高个股数量:{len(institution_new_high)}\t全市场创新高个股详情:{all_new_high}")
+    logging.warning(
+        f"全市场创新高个股数量:{len(all_new_high)}\t机构动量榜个股数量:{len(result)}\t创新高个股数量:{len(institution_new_high)}\t全市场创新高个股详情:{all_new_high}")
     industrys = [i['industry'] for i in result]
     industrySet = set(industrys)
     target = []
@@ -176,7 +182,7 @@ def run():
     for i in industrySet:
         count = industrys.count(i)
         total_count = t[i]
-        tmp = {'industry': i, 'score': round(count*count/total_count, 2),
+        tmp = {'industry': i, 'score': round(count * count / total_count, 2),
                'pool': [f"{j['code']}-{j['name']}" for j in result if j['industry'] == i]}
         target.append(tmp)
     target = sorted(target, key=lambda x: x['score'], reverse=True)
@@ -200,8 +206,10 @@ def run_v2():
         i['momentumPool'] = []
         i['pool'] = select_composition_stock(i['code'])
         tmp_name = [i['name'] for i in i['pool']]
-        i['momentumPool'] = [f"{i['code']}-{i['name']}" for i in result if i['name'] in tmp_name]
-        i['score'] = round(len(i['momentumPool'])*len(i['momentumPool'])/len(i['pool']), 2)
+        i['momentumPool'] = [
+            f"{i['code']}-{i['name']}" for i in result if i['name'] in tmp_name]
+        i['score'] = round(len(i['momentumPool']) *
+                           len(i['momentumPool']) / len(i['pool']), 2)
     CONCEPT_LIST = sorted(CONCEPT_LIST, key=lambda x: x['score'], reverse=True)
     for i in CONCEPT_LIST:
         del i['pool']
@@ -217,7 +225,10 @@ def run_v3():
         kline = get_concept_kline(i['code'])
         i['applies'] = round((kline[-1] - kline[0]) / kline[0] * 100, 2)
         i['pool'] = select_composition_stock(i['code'])
-    industry_list = sorted(industry_list, key=lambda x: x['applies'], reverse=True)
+    industry_list = sorted(
+        industry_list,
+        key=lambda x: x['applies'],
+        reverse=True)
     for i in industry_list:
         logging.warning(i)
 
@@ -230,26 +241,47 @@ def find_new_high_stock():
     count = 1
     for i in data:
         kline = i['kline']
-        if len(kline) > (N+M):
-            highest = max([i['high'] for i in kline[-(N+M):-N]])
+        if len(kline) > (N + M):
+            highest = max([i['high'] for i in kline[-(N + M):-N]])
         else:
             highest = max([i['high'] for i in kline])
         if max([i['high'] for i in kline[-N:]]) > highest:
             del i['kline']
             del i['applies_20']
             del i['list_date']
-            # if i['industry'] in ('半导体', '电气设备', '汽车配件', '电器仪表', '元器件', '新型电力', '互联网', '专用机械', '软件服务'):
+            # if i['industry'] in ('半导体', '电气设备', '汽车配件', '电器仪表', '元器件',
+            # '新型电力', '互联网', '专用机械', '软件服务'):
             logging.warning(f"{count}\t{i}")
             target.append(i)
             count += 1
     return target
 
 
+def find_new_low_stock():
+    # 寻找过去N天内不再创新低的个股
+    N, M = 50, 250
+    target = []
+    data = readMarketDataFromLocal()
+    institutions = find_institutions_holding()
+    institutions = [i for i in institutions]
+    count = 1
+    for i in data:
+        if len(i['kline']) > M and i['code'] in institutions:
+            kline = MA_V2(i['kline'], 150)
+            if kline[-1]['ma150'] < kline[-2]['ma150']:
+                lowest = min([i['low'] for i in kline[:-N]])
+                lowest_latest = min([i['low'] for i in kline[-N:]])
+                if lowest_latest > lowest:
+                    del i['kline']
+                    del i['list_date']
+                    logging.warning(f"{count}\t{i}")
+                    target.append(i)
+                    count += 1
+    return target
+
+
 if __name__ == "__main__":
     # saveMarketData2Redis()
     # saveMarketData2Local()
-    run()
-    # run_v2()
-    # run_v3()
-
-
+    # run()
+    find_new_low_stock()
